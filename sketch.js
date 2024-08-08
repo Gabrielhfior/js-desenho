5ba65be1cb09d0f23ef369eed176bcb71ce018a8
Gabriel Henrique <138537732+Gabrielhfior@users.noreply.github.com>
2024-08-08 11:22:17 -0300

    adicionando js e css

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+    createCanvas(1400, 750);
+    background("darkgreen");
+  }
+  
+  function draw() {
+    
+    stroke ("black")
+    fill ("brown")
+    
+    
+      if (mouseIsPressed) {
+        circle (mouseX, mouseY, 40, 100);}
+  
+    
+    
+    
+  }
